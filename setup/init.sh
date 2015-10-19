1e501f2f19f00ce305f24517574f86aa322ecb4b
David Ochoa <ochoaqdavid@gmail.com>
2015-10-20 00:57:08 +0300

    Fix issues with init

--- setup/init.sh
+++ setup/init.sh
@@ -12,9 +12,10 @@ DOTFILES_ROOT=$(pwd -P)
 
 source setup/init-functions.sh
 
+################################
+### XCode Command Line Tools ###
 
-### XCode Command Line Tools
-#   thx  https://github.com/alrra/dotfiles/blob/c2da74cc333/os/os_x/install_applications.sh#L39
+# Thanks  https://github.com/alrra/dotfiles/blob/c2da74cc333/os/os_x/install_applications.sh#L39
 
 if [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; then
   xcode-select --install &> /dev/null
@@ -34,14 +35,16 @@ fi
 
 ##
 
+
 source setup/brew.sh
 source setup/brew-cask.sh
+
 
 ##################
 ### Symlinking ###
 
 ### root files
-symlink "$HOME" "$DOTFILES_ROOT/bla" '.*'
+symlink "$HOME" "$DOTFILES_ROOT" '.*'
 
 ### preferences
 symlink "$HOME/Library/Preferences" "$DOTFILES_ROOT/sys/user-preferences" '*'
@@ -49,31 +52,32 @@ symlink "$HOME/Library/Preferences" "$DOTFILES_ROOT/sys/user-preferences" '*'
 ### Sublime Text 2
 symlink "$HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages" "$DOTFILES_ROOT/application-support/Sublime\ Text\ 2/Packages" '*'
 
+
 ###############
 ### Copying ###
 
 ### fonts
-cp ~/dotfiles/fonts/* ~/Library/Fonts/
+cp -a $DOTFILES_ROOT/fonts/* ~/Library/Fonts/
 success '\033[0;33mcopied\033[0m Fonts'
 
 ### zsh theme
-cp ~/dotfiles/zsh/themes/david-custom.zsh-theme ~/.oh-my-zsh/themes/david-custom.zsh-theme
+cp -a $DOTFILES_ROOT/zsh/themes/david-custom.zsh-theme ~/.oh-my-zsh/themes/david-custom.zsh-theme
 success '\033[0;33mcopied\033[0m zsh theme'
 
 ### Applications
-cp ~/dotfiles/apps/* /Applications/
+cp -a $DOTFILES_ROOT/apps/* /Applications/
 success '\033[0;33mcopied\033[0m Applications'
 
 ### System Preferences
-cp ~/dotfiles/sys/system-preferences/* /Library/Preferences/
+cp -a $DOTFILES_ROOT/sys/system-preferences/* /Library/Preferences/
 success '\033[0;33mcopied\033[0m System Preferences'
 
 ### System Configurations
-cp ~/dotfiles/sys/system-configurations/* /Library/Preferences/SystemConfiguration/
+cp -a $DOTFILES_ROOT/sys/system-configurations/* /Library/Preferences/SystemConfiguration/
 success '\033[0;33mcopied\033[0m System Configurations'
 
 ### Alfred 2
-cp ~/dotfiles/sys/application-support/Alfred\ 2/Alfred.alfredpreferences ~/Library/Application\ Support/Alfred\ 2/Alfred.alfredpreferences
+cp -a $DOTFILES_ROOT/sys/application-support/Alfred\ 2/Alfred.alfredpreferences ~/Library/Application\ Support/Alfred\ 2/Alfred.alfredpreferences
 success '\033[0;33mcopied\033[0m Alfred 2 preferences'
 
 echo ''
