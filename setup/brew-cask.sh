ce855ebb3d8fc2991f39941c18c612ee68cbde16
David Ochoa <ochoaqdavid@gmail.com>
2015-10-20 00:55:44 +0300

    Fix issues with brew-cask, and updates to list

--- setup/brew-cask.sh
+++ setup/brew-cask.sh
@@ -2,42 +2,51 @@
 
 info 'installing Applications for you.'
 
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
 # Install native apps
 brew install caskroom/cask/brew-cask
 
-brew cask install 1password
-brew cask install alfred
+# brew cask install 1password
+# brew cask install alfred
 brew cask install android-file-transfer
-# brew cask install android-studio
+brew cask install android-studio
 brew cask install appcleaner
 brew cask install cyberduck
-brew cask install dropbox
+# brew cask install dropbox
 brew cask install gpgtools
 brew cask install github-desktop
-brew cask install google-chrome
-brew cask install iterm2
+# brew cask install google-chrome
+# brew cask install iterm2
 brew cask install noizio
 brew cask install pgadmin3
-# brew cask install pixlr
-brew cask install postgres
+# brew cask install pixlr (just reference)
+# brew cask install postgres
 brew cask install postico
-# brew cask install psequel
+# brew cask install psequel (just reference)
 brew cask install rdm
 brew cask install screenhero
 brew cask install skitch
 brew cask install skype
-brew cask install slack
-# brew cask install speak
+# brew cask install slack
+# brew cask install speak (just reference)
 brew cask install spectacle
-brew cask install spotify
-brew cask install sublime-text
+# brew cask install spotify
+# brew cask install sublime-text
 brew cask install teamviewer
-# brew cask install the-unarchiver
-brew cask install toggldesktop
+# brew cask install the-unarchiver (just reference)
+# brew cask install toggldesktop
 brew cask install tunnelblick
 # brew cask install unity
 brew cask install vlc
 brew cask install virtualbox
+brew cask install wkhtmltopdf
 
 # Alternate versions of Casks for homebrew-cask (beta, etc)
 # brew tap caskroom/versions
+
+# Apps without caskroom (to keep in mind).
+# - pastebox
+# - pocket
+# - Sketchbook (just reference)
+# - Split&Concat (just reference)
