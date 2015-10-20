6e974d9c9ed31390305c2c001a283b9540211853
David Ochoa <ochoaqdavid@gmail.com>
2015-10-21 01:34:05 +0300

    Add dev.sh with development setup specifics

---
+++ setup/dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+info 'setting up dev environment for you'
+
+# Makes rbenv transparently aware of project-specific binstubs created by bundler.
+bundle install --binstubs .bundle/bin
+
+# Make sure that openssl is linked to /usr/local/bin
+brew link --force openssl
+
+# Add redis plist to OSX LaunchAgents, reference: https://gist.github.com/tonypujals/9631143.
+ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
+
+# Setup vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-ocean.vim > ~/.vim/colors/base16-ocean.vim
+pip install --user powerline-status # Install powerline-status
+brew install vim --env-std --override-system-vi # Install vim
+vim +PluginInstall
