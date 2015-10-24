9026304ac57c26fa39dc8e128fb9f6a623f9b5c5
David Ochoa <ochoaqdavid@gmail.com>
2015-10-25 02:16:52 +0300

    Update init with proper copying of preferences

--- setup/init.sh
+++ setup/init.sh
@@ -48,39 +48,40 @@ source setup/brew-cask.sh
 ### root files
 symlink "$HOME" "$DOTFILES_ROOT" '.*'
 
-### preferences
-symlink "$HOME/Library/Preferences" "$DOTFILES_ROOT/sys/user-preferences" '*'
-
 
 ###############
 ### Copying ###
 
-### fonts
-cp -a $DOTFILES_ROOT/fonts/* ~/Library/Fonts/
+### Fonts
+cp -av $DOTFILES_ROOT/fonts/ $HOME/Library/Fonts/
 success '\033[0;33mcopied\033[0m Fonts'
 
 ### zsh theme
-cp -a $DOTFILES_ROOT/zsh/themes/david-custom.zsh-theme ~/.oh-my-zsh/themes/
+cp -av $DOTFILES_ROOT/zsh/themes/david-custom.zsh-theme $HOME/.oh-my-zsh/themes/david-custom.zsh-theme
 success '\033[0;33mcopied\033[0m zsh theme'
 
 ### Applications
-cp -a $DOTFILES_ROOT/apps/* /Applications/
+cp -av $DOTFILES_ROOT/apps/ /Applications/
 success '\033[0;33mcopied\033[0m Applications'
 
+### User Preferences
+cp -av $DOTFILES_ROOT/sys/user-preferences/ $HOME/Library/Preferences/
+success '\033[0;33mcopied\033[0m User Preferences'
+
 ### System Preferences
-cp -a $DOTFILES_ROOT/sys/system-preferences/* /Library/Preferences/
+cp -av $DOTFILES_ROOT/sys/system-preferences/ /Library/Preferences/
 success '\033[0;33mcopied\033[0m System Preferences'
 
 ### System Configurations
-cp -a $DOTFILES_ROOT/sys/system-configurations/* /Library/Preferences/SystemConfiguration/
+cp -av $DOTFILES_ROOT/sys/system-configurations/ /Library/Preferences/SystemConfiguration/
 success '\033[0;33mcopied\033[0m System Configurations'
 
 ### Alfred 2
-cp -a $DOTFILES_ROOT/sys/application-support/Alfred\ 2/Alfred.alfredpreferences ~/Library/Application\ Support/Alfred\ 2/
+cp -av $DOTFILES_ROOT/sys/application-support/Alfred\ 2/Alfred.alfredpreferences $HOME/Library/Application\ Support/Alfred\ 2/
 success '\033[0;33mcopied\033[0m Alfred 2 preferences'
 
 ### Sublime Text 2
-cp -a $DOTFILES_ROOT/sys/application-support/Sublime\ Text\ 2/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/
+cp -av $DOTFILES_ROOT/sys/application-support/Sublime\ Text\ 2/Packages/User $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/
 success '\033[0;33mcopied\033[0m Sublime Text 2 preferences'
 
 
